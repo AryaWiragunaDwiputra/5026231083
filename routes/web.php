918342a576a838d03416458983a5c3c2fa365124
AryaWiragunaDwiputra <dwiputraarya31@gmail.com>
2025-06-03 23:58:44 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dosen',[DosenController::class,'index']);
+Route::get('welcome',[DosenController::class,'welcome']);
+Route::get('/pegawai/{nama}',[PegawaiController::class,'index'],'PegawaiController@index');
+Route::get('/formulir',[PegawaiController::class,'formulir']);
+Route::post('/formulir/proses',[PegawaiController::class,'proses']);
+
+// route blog
+Route::get('/blog',[BlogController::class,'home']);
+Route::get('/blog/tentang',[BlogController::class,'tentang']);
+Route::get('/blog/kontak',[BlogController::class,'kontak']);
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+/*Route::get('blog', function () {
+	return view('blog');
+});*/
 
 Route::get('frontend', function () {
     return view('frontend');
